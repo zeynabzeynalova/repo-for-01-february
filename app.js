b7c4624fb3934ebcb03e4e6a286165ae681084d6
zeynab <zzeynab694@gmail.com>
2025-02-10 21:38:56 +0400

    i added console log

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-let birinci eded=13
-let ikinci eded=12
-let toplam_sonrasi=0
-let cixma_sonrasi=0
-let vurma_sonrasi=0
-let bolme_sonrasi=0
+//variables
+let birinci_eded=13;
+let ikinci_eded=12;
+let toplam_sonrasi=0;
+let cixma_sonrasi=0;
+let vurma_sonrasi=0;
+let bolme_sonrasi=0;
 
-function topla(){
-    topla=birinci eded+ikinci eded
-    document.getElementById(netice).neticemiz+toplam sonrasi
+
+// function for sum+
+function sum(){
+toplam_sonrasi=birinci_eded+ikinci_eded;
+console.log(toplam_sonrasi)
 }
-function(){
-    cixma_sonrasi=birinci eded-ikinci eded
-    document.getElementById(cixma).innerHTML=neticemiz-cixma_sonrasi
-}
-function(){
-    bolme sonrasi= birinci eded/ikinci eded.
-    document.getElementById(bolme).innerHTML=neticemiz/bolme_sonrasi
- function(){
-    vurma_sonrasi=birinci eded x ikinci eded
-     document.getElementById(vurma).innerHTML=neticemiz-cixma_sonrasi
-    }
+
+// function for subtract-
+
+// function for devide/
+
+// function for multiple *
+ 
