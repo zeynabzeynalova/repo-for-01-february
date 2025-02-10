3b1f42106e48eea6af4a07ada506089e483068c9
zeynab <zzeynab694@gmail.com>
2025-02-10 20:49:03 +0400

    ..

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 let birinci eded=13
 let ikinci eded=12
-let toplam_sonrasi=0
-let cixma_sonrasi=0
-let vurma_sonrasi=0
-let bolme_sonrasi=2
+let toplam_sonrasi=25
+let cixma_sonrasi=1
+let vurma_sonrasi=156
+let bolme_sonrasi=1.083333
 
 function topla(){
     topla=birinci eded+ikinci eded
-    document.getElementById(netice).innerHTML=toplam sonrasi
+    document.getElementById(netice).innerHTML=toplam_sonrasi
 }
 function(){
-    cixma sonrasi=birinci eded-ikinci eded
-    document.getElementById(cixma).innerHTML=cixma
+    cixma_sonrasi=birinci eded-ikinci eded
+    document.getElementById(cixma).innerHTML=cixma_sonrasi
 }
 function(){
-    bolme sonrasi= birinci eded-ikinci eded.
-    document.getElementById(bolme).innerHTML=bolme
+    bolme sonrasi= birinci eded/ikinci eded.
+    document.getElementById(bolme).innerHTML=bolme_sonrasi
+ function(){
+    vurma_sonrasi=birinci eded x ikinci eded
+     document.getElementById(vurma).innerHTML=vurma_sonrasi
+    }
