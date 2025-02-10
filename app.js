13ef670e001cc29598b24dabde3cd2a26ac1a6f2
zeynab <zzeynab694@gmail.com>
2025-02-10 20:42:42 +0400

    ..

---
+++ app.js
@@ -0,0 +1,18 @@
+let birinci eded=13
+let ikinci eded=12
+let toplam_sonrasi=0
+let cixma_sonrasi=0
+let vurma_sonrasi=0
+let bolme_sonrasi=2
+
+function topla(){
+    topla=birinci eded+ikinci eded
+    document.getElementById(netice).innerHTML=toplam sonrasi
+}
+function(){
+    cixma sonrasi=birinci eded-ikinci eded
+    document.getElementById(cixma).innerHTML=cixma
+}
+function(){
+    bolme sonrasi= birinci eded-ikinci eded.
+    document.getElementById(bolme).innerHTML=bolme
